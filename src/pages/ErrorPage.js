98e0fa2b21787579d0696633e408feb148fabd8a
DESKTOP-S6IR4FG\nolu8 <noh8085@gmail.com>
2020-10-05 22:14:41 +0900

    project init

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ErrorPage extends Component {
+  render() {
+    return <div>404에러</div>;
+  }
+}
+
+export default ErrorPage;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="page-header">학습노트 리스트</h1>
+        <div className="main list-container contents">
+          <ul>
+            <li>
+              <div className="post-title">타이틀</div>
+              <div className="post-contents">제목</div>
+              <div className="post-time">
+                2020-09-18
+                <i className="icon ion-md-create"></i>
+                <i className="icon ion-md-trash"></i>
+              </div>
+            </li>
+          </ul>
+        </div>
+        {/* <div className="empty-content">
+          <h3>등록된 학습노트가 없습니다.</h3>
+        </div> */}
+        <NavLink to={"/write"} className="create-button">
+          <i className="ion-md-add"></i>
+        </NavLink>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
